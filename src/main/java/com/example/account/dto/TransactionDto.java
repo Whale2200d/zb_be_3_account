106e5251acf53116b3f28acefb0ffed8ccc430e4
lgr-2024 <lgr@devtools.kr>
2025-07-11 00:11:31 +0900

    transaction-balance_2

---
+++ src/main/java/com/example/account/dto/TransactionDto.java
@@ -0,0 +1,22 @@
+package com.example.account.dto;
+
+import com.example.account.type.TransactionResultType;
+import com.example.account.type.TransactionType;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TransactionDto {
+    private String accountNumber;
+    private TransactionType transactionType;
+    private TransactionResultType transactionResultType;
+    private Long amount;
+    private Long balanceSnapShot;
+    private String transactionId;
+    private LocalDateTime transactedAt;
+}
